543d54561f23bae115d6e1d47d2fc29bc8442dfd
Sridevi <jsridevi@gmail.com>
2015-10-11 08:58:20 +0200

    changed the initial code to read File using the "Files" class from Java 8.

--- src/main/java/ReadAndCompareImpl.java
+++ src/main/java/ReadAndCompareImpl.java
@@ -9,32 +9,42 @@ import java.nio.file.Files;
 import java.nio.charset.*;
 
 
+
 public class ReadAndCompareImpl implements ReadAndCompare {
 
-    public void readCompareText(Path inpPath, Path refPath) throws IOException
-        {
-            Charset cs = Charset.defaultCharset();
+    public void readCompareText(Path inpPath, Path refPath) throws IOException {
+        Charset cs = Charset.defaultCharset();
+
+        /* #1
+            below try-catch block is using
+            package: java.nio.file
+            class: Files
+            method : newBufferedReader and return type of this method is BufferedReader
+            */
+
             try {
                 BufferedReader br1 = Files.newBufferedReader(inpPath, cs);
+//                Stream<String> in = Files.lines(inpPath, cs); (not right)
                 System.out.println("\nRead first file from : " + inpPath.toString());
 
                 BufferedReader br2 = Files.newBufferedReader(refPath, cs);
+//                Stream<String> ref = Files.lines(refPath, cs);(not right)
                 System.out.println("Read second file from : " + refPath.toString());
 
                 String cLInput, cLReference;
                 while ((cLInput = br1.readLine()) != null && (cLReference = br2.readLine()) != null) {
+
                     if (!cLInput.equals(cLReference)) {
                         System.out.println("Comparison UnSuccessful.");
                         System.out.println("Current line in Input Text File: " + cLInput);
                         System.out.println("Current Line in Referenced Text File: " + cLReference);
-                    } else {
+                    }
+                    else {
                         System.out.println("Comparison Successful.");
                     }
                 }
             }catch (IOException e){
                 e.printStackTrace();
             }}
-//    public static void main(String[] args) {
-//
-//    }
+    public static void main(String[] args) {}
 }//end of Class
